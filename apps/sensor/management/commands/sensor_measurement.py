c1161cd395c21c75ec8ad80e5bb8e38ca6bb6571
M.maeda <petit.chat.0509@gmail.com>
2022-01-29 22:04:56 +0900

    [sensor]カスタムコマンド修正

--- apps/sensor/management/commands/sensor_measurement.py
+++ apps/sensor/management/commands/sensor_measurement.py
@@ -8,6 +8,6 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         bme280 = cgsensor.BME280(i2c_addr=0x77)
         bme280.forced()
-        Measurement.objects.create(Type=Measurement.Type.TEMPERATURE, value=bme280.temperature)
-        Measurement.objects.create(Type=Measurement.Type.HUMIDITY, value=bme280.humidity)
-        Measurement.objects.create(Type=Measurement.Type.PRESSURE, value=bme280.pressure)
+        Measurement.objects.create(type=Measurement.Type.TEMPERATURE, value=bme280.temperature)
+        Measurement.objects.create(type=Measurement.Type.HUMIDITY, value=bme280.humidity)
+        Measurement.objects.create(type=Measurement.Type.PRESSURE, value=bme280.pressure)
